a9fd47125365a409c610d0f0076bc747782a2967
sanggyunbak2856 <baksanggyun2856@gmail.com>
2023-03-24 12:17:24 +0900

    feature : 유저 업데이트, 삭제 기능 추가

--- src/main/java/com/example/demo/web/service/UserService.java
+++ src/main/java/com/example/demo/web/service/UserService.java
@@ -6,8 +6,10 @@ import com.example.demo.web.converter.UserConverter;
 import com.example.demo.web.dto.IdResponseDto;
 import com.example.demo.web.dto.user.UserResponseDto;
 import com.example.demo.web.dto.user.UserSaveDto;
+import com.example.demo.web.dto.user.UserUpdateDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +17,7 @@ public class UserService {
     private final UserRepository userRepository;
     private final UserConverter userConverter;
 
+    @Transactional
     public UserResponseDto findById(Long id) throws IllegalArgumentException {
         User user = userRepository.findById(id).orElseThrow(() -> {
             throw new IllegalArgumentException("User not found");
@@ -22,9 +25,24 @@ public class UserService {
         return userConverter.entityToUserResponseDto(user);
     }
 
+    @Transactional
     public IdResponseDto save(UserSaveDto userSaveDto) {
         User user = userConverter.userSaveDtoToEntity(userSaveDto);
         User savedUser = userRepository.save(user);
         return IdResponseDto.builder().id(savedUser.getId()).build();
+    }
+
+    @Transactional
+    public IdResponseDto update(Long id, UserUpdateDto userUpdateDto) throws IllegalArgumentException {
+        User user = userRepository.findById(id).orElseThrow(() -> {
+            throw new IllegalArgumentException("User not found");
+        });
+        userConverter.userUpdate(user, userUpdateDto);
+        return IdResponseDto.builder().id(user.getId()).build();
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        userRepository.deleteById(id);
     }
 }
