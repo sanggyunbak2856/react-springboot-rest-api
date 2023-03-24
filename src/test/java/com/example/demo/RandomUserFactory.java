8e0a2ab13ffcc91cb9e18a517a5b8c1c9b4fb74a
sanggyunbak2856 <baksanggyun2856@gmail.com>
2023-03-24 12:18:59 +0900

    feature : 랜덤 userUpdateDto 생성

--- src/test/java/com/example/demo/RandomUserFactory.java
+++ src/test/java/com/example/demo/RandomUserFactory.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import com.example.demo.domain.entity.User;
 import com.example.demo.web.dto.user.UserSaveDto;
+import com.example.demo.web.dto.user.UserUpdateDto;
 import com.github.javafaker.Faker;
 import com.github.javafaker.Name;
 import org.springframework.stereotype.Component;
@@ -24,6 +25,15 @@ public class RandomUserFactory {
         return UserSaveDto.builder()
                 .userId(faker.name().name())
                 .address(faker.address().fullAddress())
+                .email(faker.internet().emailAddress())
+                .password(faker.internet().password())
+                .build();
+    }
+
+    public static UserUpdateDto createRandomUserUpdateDto() {
+        return UserUpdateDto.builder()
+                .address(faker.address().fullAddress())
+                .userId(faker.name().name())
                 .email(faker.internet().emailAddress())
                 .password(faker.internet().password())
                 .build();
