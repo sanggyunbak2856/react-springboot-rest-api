10b3e3db8517f2c083d583dcffd1512b93a67a04
sanggyunbak2856 <baksanggyun2856@gmail.com>
2023-03-24 12:18:04 +0900

    feature : 유저 수정 dto로 업데이트

--- src/main/java/com/example/demo/web/converter/UserConverter.java
+++ src/main/java/com/example/demo/web/converter/UserConverter.java
@@ -3,6 +3,7 @@ package com.example.demo.web.converter;
 import com.example.demo.domain.entity.User;
 import com.example.demo.web.dto.user.UserResponseDto;
 import com.example.demo.web.dto.user.UserSaveDto;
+import com.example.demo.web.dto.user.UserUpdateDto;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -24,5 +25,12 @@ public class UserConverter {
                 .email(userSaveDto.getEmail())
                 .address(userSaveDto.getAddress())
                 .build();
+    }
+
+    public void userUpdate(User user, UserUpdateDto userUpdateDto) {
+        user.setEmail(userUpdateDto.getEmail());
+        user.setUserId(userUpdateDto.getUserId());
+        user.setAddress(userUpdateDto.getAddress());
+        user.setPassword(userUpdateDto.getPassword());
     }
 }
