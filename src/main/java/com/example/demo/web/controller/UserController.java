6abb62a21ed098551ccca4a26c966d67138fbd1e
sanggyunbak2856 <baksanggyun2856@gmail.com>
2023-03-26 14:31:09 +0900

    feature : 홈화면, 회원가입화면

---
+++ src/main/java/com/example/demo/web/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.example.demo.web.controller;
+
+import com.example.demo.web.dto.IdResponseDto;
+import com.example.demo.web.dto.user.UserSaveDto;
+import com.example.demo.web.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/user")
+    public String addUser(@ModelAttribute UserSaveDto userSaveDto) {
+        return "user/addUser";
+    }
+
+    @PostMapping("/user")
+    public String saveUser(@Validated @ModelAttribute UserSaveDto userSaveDto, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            return "/user/addUser";
+        }
+
+        userService.save(userSaveDto);
+        return "/home/home";
+    }
+}
